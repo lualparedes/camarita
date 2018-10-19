9da85b5f8065a03e5e534ffa4794a66563f3ec4d
Luis Paredes <lualps94@gmail.com>
2018-10-19 18:28:34 -0400

    Added unique keys for array children

--- src/components/views/AppCameraRoll.tsx
+++ src/components/views/AppCameraRoll.tsx
@@ -8,36 +8,39 @@ import styles from './AppCameraRoll.styles';
 
 export default class AppCameraRoll extends React.Component<AppCameraRollProps, AppCameraRollState> {
 
-	render() {
-		const images = [
-			this.props.history.location.state.newPhoto,
-		];
+  render() {
+    const images = [
+      this.props.history.location.state.newPhoto,
+    ];
 
-		return (
-		  <View style={{ flex: 1 }}>
-		  	<Header title='Pictures taken'></Header>
-		  	<ScrollView contentContainerStyle={ styles.scrollView }>
-		  		{
-		  			images.map((img) => (
-		  				<View style={ styles.picture }>
-		  					<TouchableHighlight style={ styles.deleteIcon }>
-		  						<Image
-		  							source={ require('../../assets/img/icon_delete.png') }
-		  							style={ styles.deleteIcon__img }
-		  						/>
-		  					</TouchableHighlight>
-		  					<Image
-		  						source={{ uri: img }}
-		  						style={ styles.picture__content }
-		  						resizeMode="contain"
-		  					/>
-		  				</View>
-		  			))
-		  		}
-		  	</ScrollView>
-		  </View>
-		);
-	}
+    return (
+      <View style={{ flex: 1 }}>
+        <Header title='Pictures taken'></Header>
+        <ScrollView contentContainerStyle={ styles.scrollView }>
+          {
+            images.map((img, i) => (
+              <View
+                style={ styles.picture }
+                key={ `image-${i}` }
+              >
+                <TouchableHighlight style={ styles.deleteIcon }>
+                  <Image
+                    source={ require('../../assets/img/icon_delete.png') }
+                    style={ styles.deleteIcon__img }
+                  />
+                </TouchableHighlight>
+                <Image
+                  source={{ uri: img }}
+                  style={ styles.picture__content }
+                  resizeMode="contain"
+                />
+              </View>
+            ))
+          }
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const AppCameraRollWithRouter = withRouter(AppCameraRoll);
