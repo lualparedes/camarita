3b154cf9313fa613724ecf7be44c1a8806e49b85
Luis Paredes <lualps94@gmail.com>
2018-11-07 14:16:45 -0400

    Parameterized styles for Camera

--- src/components/views/Camera.styles.tsx
+++ src/components/views/Camera.styles.tsx
@@ -1,13 +1,20 @@
 import { StyleSheet } from 'react-native';
 
+import constants from '../../assets/styles/config';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-end',
     padding: 48,
+  },
+  'container--light': {
+    backgroundColor: constants.themeLight.COLOR_BG,
+  },
+  'container--dark': {
+    backgroundColor: constants.themeDark.COLOR_BG,
   },
   trigger: {
     width: 64,
