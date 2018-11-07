46c47f7cf68c70c7b56a045fb88f261aef5ea677
Luis Paredes <lualps94@gmail.com>
2018-11-07 16:25:50 -0400

    Parameterized styles for ButtonMain

--- src/components/atoms/ButtonMain.styles.tsx
+++ src/components/atoms/ButtonMain.styles.tsx
@@ -4,11 +4,16 @@ import constants from '../../assets/styles/config';
 
 const styles = StyleSheet.create({
   buttonMain: {
-    backgroundColor: constants.themeLight.COLOR_ACCENT_2,
     borderRadius: 24,
     height: 48,
     paddingVertical: 14,
     paddingHorizontal: 32,
+  },
+  'buttonMain--light': {
+    backgroundColor: constants.themeLight.COLOR_ACCENT_2,
+  },
+  'buttonMain--dark': {
+    backgroundColor: constants.themeDark.COLOR_ACCENT_2,
   },
   buttonMain__text: {
     color: '#ffffff',
