b3465b226adc5217d57b857219c4b011a1893b2e
Luis Paredes <lualps94@gmail.com>
2019-01-10 18:48:16 -0400

    Changed entry point for the app to run

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-/// <reference path="./App.interfaces.d.ts" />
+/// <reference path="./src/App.interfaces.d.ts" />
 import * as React from 'react';
 import { View } from 'react-native';
 import { Font } from 'expo';
 import { Router, Route, Link } from 'react-router-native';
 import createHistory from 'history/createMemoryHistory';
 
-import styles from './App.styles';
-import { Provider, Consumer } from './Context';
-import MainView from './components/views/MainView';
-import CameraView from './components/views/Camera';
-import AppCameraRoll from './components/views/AppCameraRoll';
-import OptionsMenu from './components/views/OptionsMenu';
+import styles from './src/App.styles';
+import { Provider, Consumer } from './src/Context';
+import MainView from './src/components/views/MainView';
+import CameraView from './src/components/views/Camera';
+import AppCameraRoll from './src/components/views/AppCameraRoll';
+import OptionsMenu from './src/components/views/OptionsMenu';
 
 const history = createHistory();
 
@@ -36,8 +36,8 @@ export default class App extends React.Component<AppProps, AppState> {
 
   componentDidMount() {
     Font.loadAsync({
-      'Muli-Black': require('./assets/fonts/Muli/Muli-Black.ttf'),
-      'Muli': require('./assets/fonts/Muli/Muli-Regular.ttf'),
+      'Muli-Black': require('./src/assets/fonts/Muli/Muli-Black.ttf'),
+      'Muli': require('./src/assets/fonts/Muli/Muli-Regular.ttf'),
     })
     .then(() => {
       this.setState({ loadedAssets: true });
