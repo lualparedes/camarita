164ae9f817603dfe53507a4065619ebaf2af4574
Luis Paredes <lualps94@gmail.com>
2019-02-01 16:22:52 -0400

    Refactored ButtonMain.spec for improved readability of test output

--- src/tests/components/atoms/ButtonMain.spec.tsx
+++ src/tests/components/atoms/ButtonMain.spec.tsx
@@ -2,24 +2,49 @@ import * as React from 'react';
 import ButtonMain from '../../../components/atoms/ButtonMain';
 import { Provider } from '../../../Context';
 import { StaticRouter } from 'react-router-native';
-import * as renderer from 'react-test-renderer';
+import TestRenderer from 'react-test-renderer';
+import { Text } from 'react-native';
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(
-    <Provider
-      value={{
-        theme: 'light',
-        toggleTheme: jest.mock()
-      }}
-    >
-      <StaticRouter context={{}}>
-        <ButtonMain
-          to="test-route"
-          label="Test"
-          theme="light"
-        />
-      </StaticRouter>
-    </Provider>
-    ).toJSON();
-  expect(rendered).toBeTruthy();
+describe('ButtonMain component', () => {
+
+  it('renders without crashing', () => {
+    const rendered = TestRenderer.create(
+      <Provider
+        value={{
+          theme: 'light',
+          toggleTheme: jest.mock()
+        }}
+      >
+        <StaticRouter context={{}}>
+          <ButtonMain
+            to="test-route"
+            label="Test"
+            theme="light"
+          />
+        </StaticRouter>
+      </Provider>
+      ).toJSON();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('has one Text descendant', () => {
+    const testRenderer = TestRenderer.create(
+      <Provider
+        value={{
+          theme: 'light',
+          toggleTheme: jest.mock()
+        }}
+      >
+        <StaticRouter context={{}}>
+          <ButtonMain
+            to="test-route"
+            label="Test"
+            theme="light"
+          />
+        </StaticRouter>
+      </Provider>
+    );
+    const testInstance = testRenderer.root;
+    expect(testInstance.findAllByType(Text).length).toBe(1);
+  });
 });
